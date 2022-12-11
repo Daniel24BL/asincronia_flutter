14fc66f9ebbc3ecdad168652455278a12dee205b
Daniel Baque <daniel-baque98@outlook.com>
2022-12-11 15:03:13 -0500

    App de asicronía en Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,153 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:asincronia_flutter/services/mockapi.dart';
 
-void main() {
+Future<void> main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int textorayo = 0;
+  double anchorayo = 0;
+  int segundosrayo = 0;
+  int rayo = 0;
+  int textovehiculo = 0;
+  double anchovehiculo = 0;
+  int segundosvehiculo = 0;
+  int vehiculo = 0;
+  int textopersona = 0;
+  double personaancho = 0;
+  int personasegundos = 0;
+  int persona = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      debugShowCheckedModeBanner: false,
+      title: 'Daniel Baque 7moB',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("AsincroniaFlutter Daniel Baque",
+              style: TextStyle(color: Colors.white)),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: FractionallySizedBox(
+          widthFactor: 1,
+          heightFactor: 1,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  FloatingActionButton(
+                    onPressed: () async {
+                      setState(() {
+                        anchorayo = 200;
+                        segundosrayo = 1;
+                      });
+                      rayo = await MockApi().getFerrariInteger();
+                      setState(() {
+                        textorayo = rayo;
+                        anchorayo = 0;
+                        segundosrayo = 0;
+                      });
+                    },
+                    backgroundColor: Colors.green,
+                    child: const Icon(
+                      Icons.flash_on,
+                      color: Colors.black,
+                    ),
+                  ),
+                  AnimatedContainer(
+                    width: anchorayo,
+                    height: 15,
+                    decoration: const BoxDecoration(color: Colors.green),
+                    duration: Duration(seconds: segundosrayo),
+                  ),
+                  Text(
+                    textorayo.toInt().toString(),
+                    style: const TextStyle(color: Colors.green, fontSize: 25.0),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  FloatingActionButton(
+                      onPressed: () async {
+                        setState(() {
+                          anchovehiculo = 200;
+                          segundosvehiculo = 3;
+                        });
+                        vehiculo = await MockApi().getHyundaiInteger();
+                        setState(() {
+                          textovehiculo = vehiculo;
+                          anchovehiculo = 0;
+                          segundosvehiculo = 0;
+                        });
+                      },
+                      backgroundColor: Colors.yellow,
+                      child: const Icon(
+                        Icons.airport_shuttle,
+                        color: Colors.black,
+                      )),
+                  AnimatedContainer(
+                    width: anchovehiculo,
+                    height: 15,
+                    decoration: const BoxDecoration(color: Colors.yellow),
+                    duration: Duration(seconds: segundosvehiculo),
+                  ),
+                  Text(
+                    textovehiculo.toInt().toString(),
+                    style:
+                        const TextStyle(color: Colors.yellow, fontSize: 25.0),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  FloatingActionButton(
+                      onPressed: () async {
+                        setState(() {
+                          personaancho = 200;
+                          personasegundos = 10;
+                        });
+                        persona = await MockApi().getFisherPriceInteger();
+                        setState(() {
+                          textopersona = persona;
+                          personaancho = 0;
+                          personasegundos = 0;
+                        });
+                      },
+                      backgroundColor: Colors.red,
+                      child: const Icon(
+                        Icons.directions_walk,
+                        color: Colors.black,
+                      )),
+                  AnimatedContainer(
+                    width: personaancho,
+                    height: 15,
+                    decoration: const BoxDecoration(color: Colors.red),
+                    duration: Duration(seconds: personasegundos),
+                  ),
+                  Text(
+                    textopersona.toInt().toString(),
+                    style: const TextStyle(color: Colors.red, fontSize: 25.0),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
